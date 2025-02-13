13abac73ef88745b2ef8b01e12df69dca555d4de
Federico Giorgino <59868572+federicogiorgino@users.noreply.github.com>
2025-02-13 19:17:03 +0100

    feat: add create contact form

---
+++ actions/contacts.ts
@@ -0,0 +1,55 @@
+'use server'
+
+import { currentUser } from '@clerk/nextjs/server'
+
+import { prisma } from '@/lib/prisma'
+
+import { contactSchema } from '@/schemas/job-application'
+import { ContactValues } from '@/types/job-application'
+
+export async function getContacts() {
+  try {
+    const user = await currentUser()
+
+    if (!user) {
+      return { status: 'error', error: 'Unauthorized' }
+    }
+
+    const contacts = await prisma.contact.findMany({
+      where: {
+        userId: user.id,
+      },
+      orderBy: { name: 'asc' },
+    })
+    return { status: 'success', data: contacts }
+  } catch (error) {
+    return { status: 'error', error: 'Error fetching contacts' }
+  }
+}
+
+export async function createContact(data: ContactValues) {
+  try {
+    const user = await currentUser()
+
+    if (!user) {
+      return { status: 'error', error: 'Unauthorized' }
+    }
+
+    const validated = contactSchema.safeParse(data)
+
+    if (!validated.success) {
+      return { status: 'error', error: validated.error.errors }
+    }
+
+    await prisma.contact.create({
+      data: {
+        userId: user.id,
+        ...validated.data,
+      },
+    })
+    return { status: 'success' }
+  } catch (error) {
+    console.error(error)
+    return { status: 'error', error: 'Error creating contact' }
+  }
+}
