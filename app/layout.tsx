bf2238413500349950e16113a5620c32999c3278
Federico Giorgino <59868572+federicogiorgino@users.noreply.github.com>
2025-02-13 15:04:50 +0100

    feat: add sign-up page

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Reveala.xyz',
+  title: 'Applicare',
   description: 'The job seeker best friend',
 }
 
